a4f65669ccdb17c03d111d661c7b932fad5f2c02
Luc Street <lucis-fluxum@users.noreply.github.com>
2018-05-28 20:01:27 -0700

    Finish Iterator implementation for LinkedListStack

--- src/ch1/linked_list_stack.rs
+++ src/ch1/linked_list_stack.rs
@@ -53,16 +53,40 @@ impl<T> LinkedListStack<T> {
     }
 }
 
-struct LinkedListStackIterator<'a, T: 'a> {
-    index: usize,
-    list: &'a LinkedListStack<T>
+pub struct LinkedListStackIterator<'a, T: 'a> {
+    item: Option<&'a Node<T>>,
 }
 
 impl<'a, T> Iterator for LinkedListStackIterator<'a, T> {
-    type Item = T;
+    type Item = &'a T;
 
-    fn next(&mut self) -> Option<T> {
+    fn next(&mut self) -> Option<&'a T> {
+        let value: Option<&T> = match self.item {
+            Some(ref node_ref) => node_ref.value.as_ref(),
+            None => None,
+        };
 
+        let next_item: Option<&Node<T>> = match self.item {
+            Some(ref node_ref) => match node_ref.next {
+                Some(ref next_node_ref) => Some(next_node_ref),
+                None => None,
+            },
+            None => None,
+        };
+        ::std::mem::replace(&mut self.item, next_item);
+
+        value
+    }
+}
+
+impl<'a, T> IntoIterator for &'a LinkedListStack<T> {
+    type Item = &'a T;
+    type IntoIter = LinkedListStackIterator<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        LinkedListStackIterator {
+            item: Some(&self.first),
+        }
     }
 }
 
@@ -106,7 +130,21 @@ mod tests {
     }
 
     #[test]
-    fn iteration() {
-        // TODO
+    fn iterator_implementation() {
+        let mut list = LinkedListStack::new(0);
+        list.push(1);
+        list.push(2);
+
+        // Temporary immutable borrow to check elements
+        {
+            let elements: Vec<&i32> = list.into_iter().collect();
+            assert_eq!(vec![&2, &1, &0], elements);
+        }
+
+        list.pop();
+        list.pop();
+
+        let elements: Vec<&i32> = list.into_iter().collect();
+        assert_eq!(vec![&0], elements);
     }
 }
