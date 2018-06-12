f7191fa721abef034cd06b423cf681b136e14cf2
Luc Street <lucis-fluxum@users.noreply.github.com>
2018-06-11 18:04:38 -0700

    Use HashSet instead of HashMap/Vec to avoid issues with duplicates

--- src/ch1/intersection.rs
+++ src/ch1/intersection.rs
@@ -1,27 +1,26 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 use std::hash::Hash;
 
+// Note: order is not preserved
 pub fn intersection<'a, T>(first: &'a [T], second: &'a [T]) -> Vec<&'a T>
 where
     T: Hash + Eq,
 {
     // Allocate worst-case sizes for these
-    let mut existences = HashMap::with_capacity(first.len().max(second.len()));
-    let mut intersection = Vec::with_capacity(first.len().min(second.len()));
+    let mut existences = HashSet::with_capacity(first.len().max(second.len()));
+    let mut intersection = HashSet::with_capacity(first.len().min(second.len()));
 
     for item in first {
-        if !existences.contains_key(item) {
-            existences.insert(item, true);
-        }
+        existences.insert(item);
     }
 
     for item in second {
-        if existences.contains_key(item) {
-            intersection.push(item);
+        if existences.contains(item) {
+            intersection.insert(item);
         }
     }
 
-    intersection
+    intersection.into_iter().collect()
 }
 
 #[cfg(test)]
@@ -32,10 +31,23 @@ mod tests {
     fn basic_behavior() {
         let first = [1, 2, 3, 4, 5, 6, 7];
         let second = [2, 4, 6, 8];
-        assert_eq!(vec![&2, &4, &6], intersection(&first, &second));
+        check_match(&vec![&2, &4, &6], &intersection(&first, &second));
 
         let first = ["a", "b", "c", "d"];
         let second = ["a", "c", "e"];
-        assert_eq!(vec![&"a", &"c"], intersection(&first, &second));
+        check_match(&vec![&"a", &"c"], &intersection(&first, &second));
+    }
+
+    fn check_match<T>(first: &Vec<T>, second: &Vec<T>)
+    where
+        T: Eq,
+    {
+        for element in first {
+            assert!(second.contains(element));
+        }
+
+        for element in second {
+            assert!(first.contains(element));
+        }
     }
 }
